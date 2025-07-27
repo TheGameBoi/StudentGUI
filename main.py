3b9e0ac54c59d2b2cd3ed23284732965a3cfb1aa
GameBoi <thegameboi@proton.me>
2025-07-26 21:22:53 -0500

    Added Data Loading and Displayed it on Screen.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QLabel, QWidget, QApplication, QPushButton, QComboBox, QMainWindow, QGridLayout, \
     QTableWidget, QTableWidgetItem
+import sqlite3
 import sys
 
 
@@ -22,8 +23,18 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Student Name", "Student ID", "Course", "Phone Number"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
 
 
 
@@ -31,5 +42,6 @@ class MainWindow(QMainWindow):
 
 app = QApplication(sys.argv)
 window = MainWindow()
+window.load_data()
 window.show()
 sys.exit(app.exec())
