58498b1412bc73207964f7220cbcfcc46312fb2a
GameBoi <thegameboi@proton.me>
2025-07-27 15:48:05 -0500

    Added an "Add Student" funtion and button. Updated the database.db file with a new copy.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QLabel, QWidget, QApplication, QPushButton, QComboBox, QMainWindow, QGridLayout, \
-    QTableWidget, QTableWidgetItem
+    QTableWidget, QTableWidgetItem, QDialog, QLineEdit, QVBoxLayout, QToolBar, QTextEdit
 import sqlite3
 import sys
 
@@ -10,11 +10,15 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management App")
+        self.setGeometry(500, 500, 550, 400)
 
         menu_bar = self.menuBar().addMenu("&Help")
         file_bar = self.menuBar().addMenu("&File")
 
         student_action = QAction("Add Student", self)
+        student_action.triggered.connect(self.insert)
+        self.search = QAction("Search", self)
+        file_bar.addAction(self.search)
         file_bar.addAction(student_action)
 
         about_action = QAction("About", self)
@@ -22,7 +26,7 @@ class MainWindow(QMainWindow):
 
         self.table = QTableWidget()
         self.table.setColumnCount(4)
-        self.table.setHorizontalHeaderLabels(("Student Name", "Student ID", "Course", "Phone Number"))
+        self.table.setHorizontalHeaderLabels(("Student ID", "Student Name", "Course", "Phone Number"))
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
@@ -36,6 +40,58 @@ class MainWindow(QMainWindow):
                 self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
         connection.close()
 
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.setWindowTitle("Add Student")
+        dialog.setGeometry(300, 300, 500, 200)
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(400)
+        self.setFixedHeight(100)
+
+        #Grid Layout
+        layout = QGridLayout()
+        # Input Fields
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Student Name")
+
+        self.student_number = QLineEdit()
+        self.student_number.setPlaceholderText("Student Number")
+
+        self.courses = QComboBox()
+        classes = ["Math", "Science", "History", "Engineering"]
+        self.courses.addItems(classes)
+
+
+        # Submit Button
+        add = QPushButton("Add Student")
+        add.clicked.connect(self.add_student)
+
+        # Displaying Widgets
+        layout.addWidget(self.student_name, 0, 0)
+        layout.addWidget(self.student_number, 0, 1)
+        layout.addWidget(self.courses, 0, 2)
+        layout.addWidget(add, 1, 0, 1, 2)
+        self.setLayout(layout)
+
+
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.courses.itemText(self.courses.currentIndex())
+        mobile = self.student_number.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
+            (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        window.load_data()
 
 
 
