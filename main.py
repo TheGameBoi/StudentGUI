c2a6e801a7be8431e6d56e31ec714c3e726949a1
GameBoi <thegameboi@proton.me>
2025-07-26 20:17:27 -0500

    Added Button Function to GUI.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QLabel, QWidget, QApplication, QPushButton, QComboBox, QMainWindow, QGridLayout
 import sys
 
@@ -8,8 +9,15 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Student Management App")
 
-        menu_bar = self.menuBar().addMenu("&File")
-        file_bar = self.menuBar().addMenu("&Help")
+        menu_bar = self.menuBar().addMenu("&Help")
+        file_bar = self.menuBar().addMenu("&File")
+
+        student_action = QAction("Add Student", self)
+        file_bar.addAction(student_action)
+
+        about_action = QAction("About", self)
+        menu_bar.addAction(about_action)
+
 
 
 
